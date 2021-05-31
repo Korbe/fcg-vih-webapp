fbcb1604ef9942b57e4c068abdbd3ce5fa6b8296
Korbitsch <korbitsch@meifirma.com>
2021-05-31 10:06:10 +0200

    Initial Project Setup with configuration

---
+++ webpack.mix.js
@@ -0,0 +1,29 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js').vue()
+    .copy("resources/public", "public/")
+    .sass('resources/css/app.scss', 'public/css')
+    .options({
+        postCss: [
+            require('postcss-import'),
+            require('tailwindcss'),
+        ]
+    })
+    .sourceMaps(false, 'eval')
+    .disableSuccessNotifications()
+    .webpackConfig(require('./webpack.config'));
+
+if (mix.inProduction()) {
+    mix.version();
+}
