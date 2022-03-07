a4cdbe02121d6a8d2d496a83847b8f9b9faf104b
Korbitsch <korbitsch@meifirma.com>
2022-03-07 19:20:54 +0100

    enhanced error page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,22 @@
-@extends('errors::minimal')
-
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __('Service Unavailable'))
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Maintenance - FCG Villach</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<article class="text-center mx-auto max-w-3xl py-48">
+    <img class="h-48 mx-auto my-5" src="/images/Logo-fcg-villach.png"/>
+    <h1 class="font-bold text-3xl py-3 text-gray-700">Unsere neue Webseite ist bald fertig!</h1>
+    <div>
+        <p class="text-gray-500 mb-5">Wir entschuldigen uns für die Unannehmlichkeiten, wir führen derzeit ncoh einige Arbeiten an der Seite durch. Bei Bedarf können Sie uns selbstverständlich
+            jederzeit
+            kontaktieren.</p>
+        <a class="text-blue-500  underline" href="mailto:office@fcg-villach.at">office@fcg-villach.at</a>
+        <p class="text-gray-500 mt-5">- Ihre Freie Christengemeinde in Villach</p>
+    </div>
+</article>
+</body>
+</html>
