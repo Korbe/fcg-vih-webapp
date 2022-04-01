26b67bbebcb5ea8d814927c019ef6bc0daa3eb34
Korbitsch <korbitsch@meifirma.com>
2022-04-01 02:20:49 +0200

    fixed image path in maintance mode

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <article class="text-center mx-auto max-w-3xl py-48">
-    <img class="h-48 mx-auto my-5" src="/images/Logo-fcg-villach.png"/>
+    <img class="h-48 mx-auto my-5" src="/images/fcg-villach-logo.png"/>
     <h1 class="font-bold text-3xl py-3 text-gray-700">Unsere neue Webseite ist bald fertig!</h1>
     <div>
         <p class="text-gray-500 mb-5">Wir entschuldigen uns für die Unannehmlichkeiten, wir führen derzeit noch einige Arbeiten an der Seite durch. Bei Bedarf können Sie uns selbstverständlich
