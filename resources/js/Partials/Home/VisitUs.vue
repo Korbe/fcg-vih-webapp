e9870baed918eb047a582081628129728faddbfd
Korbitsch <korbitsch@meifirma.com>
2022-07-31 23:22:15 +0200

    Extracted section

---
+++ resources/js/Partials/Home/VisitUs.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="relative bg-brand-secondary-500 mb-10">
+    <div class="h-56 bg-brand-primary-300 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+
+      <img class="w-full h-full object-cover"
+           src="https://images.unsplash.com/photo-1582880159736-8bb57334d2f5?auto=format&fit=crop&w=1920"
+           alt=""/>
+    </div>
+    <div class="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+      <div class="md:ml-auto md:w-1/2 md:pl-10">
+        <h2 class="text-base font-semibold uppercase tracking-wider text-brand-secondary-700">Sonntags</h2>
+        <p class="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">Gottesdienst</p>
+        <p class="mt-3 text-lg text-white">
+          Jeden Sonntag findet bei uns um 10 Uhr der Gottesdienst statt.
+          Beginnend mit dem Lobpreis und Anbetung Gottes stimmen wir den Gottesdienst ein. Anschließend predigt unser
+          Pastor. Für die Kleinen gibt's einen speziellen
+          Kindergottesdienst. Die Predigt wird anschließend hier als Audioformat hochgeladen. Es ist keine Anmeldung
+          erforderlich!
+        </p>
+
+        <div class="mt-3 flex text-lg text-white">
+          <ClockIcon class="text-brand-secondary-700 h-6 w-6 mr-2"/>
+          <p class="ml-2">Sonntags 10 Uhr</p>
+        </div>
+
+        <div class="mt-3 flex text-lg text-white">
+          <LocationMarkerIcon class="text-brand-secondary-700 h-6 w-6 mr-2"/>
+          <p class="ml-2">Vassacherstraße 28, 9500 Villach</p>
+        </div>
+
+        <div class="mt-8">
+          <div class="inline-flex rounded-md shadow">
+            <Link :href="route('public.events')"
+                  class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50">
+              Alle Events anschauen
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+import {
+
+    ClockIcon,
+    LocationMarkerIcon,
+
+} from "@heroicons/vue/outline";
+
+
+export default {
+  name: 'visit-uss',
+    components: {ClockIcon, LocationMarkerIcon},
+}
+</script>
---
+++ resources/js/Partials/Home/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="visitus" class="relative py-24 bg-white overflow-hidden">
+    <div class="relative px-4 sm:px-6 lg:px-8">
+      <div class="text-lg max-w-prose mx-auto">
+        <h1 id="freie-christengemeinde" style="scroll-margin: 7rem">
+          <span class="block text-base text-center text-brand-primary font-semibold tracking-wide uppercase">Freie Christengemeinde Villach</span>
+          <span
+              class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Willkommen auf unserer Homepage</span>
+        </h1>
+        <p class="mt-8 text-xl text-gray-500 leading-8">Als Freie Christengemeinde – Pfingstgemeinde ist es uns ein
+          Anliegen, den Menschen in unserem Land zu dienen, indem wir die gute
+          Nachricht von Jesus Christus gesellschaftsrelevant verkündigen und praktizieren.</p>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'welcome'
+}
+</script>
