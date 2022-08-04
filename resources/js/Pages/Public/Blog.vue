503e2e8925dc212c43b194c7234c6f4a33822213
Korbitsch <korbitsch@meifirma.com>
2022-08-04 16:12:32 +0200

    Centered hero pages

--- resources/js/Pages/Public/Blog.vue
+++ resources/js/Pages/Public/Blog.vue
@@ -1,33 +1,46 @@
 <template>
     <public-layout title="Predigten">
 
+        <div class="flex flex-col h-screen relative">
 
-        <header class="relative h-screen">
             <div class="absolute inset-0 ">
-                <img class="w-full h-full object-cover" src="/images/predigt.jpg" alt=""/>
+                <img alt="" class="w-full h-full object-cover" src="/images/predigt.jpg"/>
             </div>
 
-            <Nav hasBackground blueMobileButton/>
+            <Nav blueMobileButton hasBackground/>
 
-            <div class="relative px-4 pt-16 sm:px-6 sm:py-24 lg:pt-32 lg:px-8">
-                <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                    <span class="block text-white">Gottes Wort zum</span>
-                    {{ ' ' }}
-                    <span class="block text-brand-primary">anhören</span>
-                </h1>
-                <p class="mt-6 max-w-lg mx-auto text-center text-xl text-white sm:max-w-3xl">Hören Sie sich unsere Predigten ganz bequem an. Bleiben Sie auf dem Laufenden über die neuesten Predigten von FCG Villach.</p>
+            <div class="relative flex justify-center items-center flex-grow">
+                <header>
+                    <div class="px-4 sm:px-6 lg:px-8">
+                        <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+                            <span class="block text-white">Gottes Wort zum</span>
+                            {{ ' ' }}
+                            <span class="block text-brand-primary">anhören</span>
+                        </h1>
+                        <p class="mt-6 max-w-lg mx-auto text-center text-xl text-white sm:max-w-3xl">Hören Sie sich
+                            unsere Predigten ganz bequem an. Bleiben Sie auf dem Laufenden über die neuesten Predigten
+                            von FCG Villach.</p>
 
-                <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
-                    <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-                        <a href="#" @click.prevent="playFirstPost"
-                              class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-primary hover:bg-brand-primary-400 sm:px-8"><PlayIcon class="h-7 mr-3"/>Neueste Predigt anhören</a>
-                        <a id="all-posts" :href="route('public.blog') + '#posts'"
-                           class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-secondary hover:bg-brand-secondary-400 sm:px-8"><CollectionIcon class="h-7 mr-3"/> Alle Predigten </a>
+                        <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+                            <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+                                <a class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-primary hover:bg-brand-primary-400 sm:px-8" href="#"
+                                   @click.prevent="playFirstPost">
+                                    <PlayIcon class="h-7 mr-3"/>
+                                    Neueste Predigt anhören</a>
+                                <a id="all-posts" :href="route('public.blog') + '#posts'"
+                                   class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-secondary hover:bg-brand-secondary-400 sm:px-8">
+                                    <CollectionIcon class="h-7 mr-3"/>
+                                    Alle Predigten </a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
 
-        </header>
+                    <div class="hidden lg:flex justify-center items-center pt-32">
+                        <div class="mouse"></div>
+                    </div>
+                </header>
+            </div>
+        </div>
 
         <div id="posts" class="bg-gradient-to-b from-white to-gray-100">
             <div class="max-w-4xl mx-auto py-32 px-4">
--- resources/js/Partials/Home/Hero.vue
+++ resources/js/Partials/Home/Hero.vue
@@ -1,35 +1,47 @@
 <template>
+    <div class="flex flex-col h-screen relative">
 
-        <header class="relative h-screen">
-            <div class="absolute inset-0 ">
-                <img class="w-full h-full object-cover" src="/images/hero.jpg" alt=""/>
-            </div>
+        <div class="absolute inset-0 ">
+            <img alt="" class="w-full lg:hidden h-full object-cover" src="/images/hero.jpg"/>
+            <img alt="" class="hidden lg:block w-full h-full object-cover" src="/images/hero-lg.jpg"/>
+        </div>
 
-            <Nav hasBackground blueMobileButton/>
+        <Nav blueMobileButton hasBackground/>
 
-            <div class="relative px-4 pt-16 sm:px-6 sm:py-24 lg:pt-32 lg:px-8">
-                <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                    <span class="block text-white">Freie Christengemeinde</span>
-                    {{ ' ' }}
-                    <span class="block text-brand-primary">Villach</span>
-                </h1>
-                <p class="mt-6 max-w-lg mx-auto text-center text-xl text-white sm:max-w-3xl">Wir sind eine Gruppe christusgläubiger Menschen, die aus Freude an Jesus Christus und an ihrem neuen Leben vereint sind.</p>
+        <div class="relative flex justify-center items-center flex-grow">
+            <header>
+                <div class="px-4 sm:px-6 lg:px-8">
+                    <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+                        <span class="block text-white">Freie Christengemeinde</span>
+                        {{ ' ' }}
+                        <span class="block text-brand-primary">Villach</span>
+                    </h1>
+                    <p class="mt-6 max-w-lg mx-auto text-center text-xl text-white sm:max-w-3xl">Wir sind eine Gruppe
+                        christusgläubiger Menschen, die aus Freude an Jesus Christus und an ihrem neuen Leben vereint
+                        sind.</p>
 
-                <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
-                    <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-                        <Link :href="route('public.blog')"
-                              class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-primary hover:bg-brand-primary-400 sm:px-8"><PlayIcon class="h-7 mr-3"/> Predigten anhören </Link>
-                        <a :href="route('public.home') + '#visitus'"
-                              class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-secondary hover:bg-brand-secondary-400 sm:px-8"><UserGroupIcon class="h-7 mr-3"/> Besuchen Sie uns </a>
+                    <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+                        <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+                            <Link :href="route('public.blog')"
+                                  class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-primary hover:bg-brand-primary-400 sm:px-8">
+                                <PlayIcon class="h-7 mr-3"/>
+                                Predigten anhören
+                            </Link>
+                            <a :href="route('public.home') + '#visitus'"
+                               class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-brand-secondary hover:bg-brand-secondary-400 sm:px-8">
+                                <UserGroupIcon class="h-7 mr-3"/>
+                                Besuchen Sie uns </a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="invisible lg:visible flex items-center justify-around pt-16">
-                <div class="mouse"></div>
-            </div>
+                <div class="hidden lg:flex justify-center items-center pt-32">
+                    <div class="mouse"></div>
+                </div>
+            </header>
+        </div>
+    </div>
 
-        </header>
 </template>
 
 <script>
