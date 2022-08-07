4c465d73556f0e5fb8f3255d17324bc366d58f24
Korbitsch <korbitsch@meifirma.com>
2022-08-07 02:24:08 +0200

    margin top at donate

--- resources/js/Pages/Public/Donate.vue
+++ resources/js/Pages/Public/Donate.vue
@@ -10,7 +10,7 @@
             <div class="flex flex-grow bg-gradient-to-b from-white to-gray-100 justify-center items-center">
 
                     <div>
-                        <div class="text-center px-5">
+                        <div class="text-center mt-16 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-28 px-5">
                             <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                                 Ihre Spende <span class="text-brand-primary">hilft</span>
                             </h1>
