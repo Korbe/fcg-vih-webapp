83cc3337b94736b19d5528e40e23c55514ede0ac
Lukas Korbitsch <korbitschl@gmail.com>
2025-02-27 15:45:44 +0100

    updated vite config for rollout

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+          // Aliases für das Node-Modul 'crypto', das auf den Polyfill verweist
+          crypto: 'crypto-browserify',
+        },
+      },
+      optimizeDeps: {
+        // Wenn du das Modul explizit vorab optimieren möchtest, füge es hier hinzu
+        include: ['crypto-browserify'],
+      },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
