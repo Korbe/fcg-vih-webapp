a4bdbbd1f59a176e0a5fca30a3015d4633d9a886
Lukas Korbitsch <korbitschl@gmail.com>
2026-01-29 15:05:49 +0100

    added logo to splash screen

--- resources/js/Partials/SplashScreen.vue
+++ resources/js/Partials/SplashScreen.vue
@@ -1,7 +1,8 @@
 <template>
     <transition name="fade">
         <div v-if="show" class="fixed inset-0 flex items-center flex-col justify-center bg-white z-50">
-            <h1 class="text-3xl animate-pulse text-brand-text dark:text-brand-text-dark">FCG Villach</h1>
+            <img src="/images/fcg-villach-logo-minimal.png" alt="Logo" class="w-auto h-32 mb-4" />
+            <h1 class="text-3xl animate-pulse text-brand-text dark:text-brand-text-dark">fcg-Villach.at</h1>
         </div>
     </transition>
 </template>
